e60a8fe0778a2ac99fdd375871c58995208fcc55
kontosgeorge-sdc <geokonteac@gmail.com>
2020-01-28 16:15:39 +0000

    changed background to red

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame, time
 pygame.init()
 width, height = 800, 600
 dvdLogoSpeed = [1, 1]
-backgroundColor = 255, 255, 0
+backgroundColor = 255, 0, 0
 
 screen = pygame.display.set_mode((width, height))
 
